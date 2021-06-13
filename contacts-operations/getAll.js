eb35e4b6283bbb47174b7b5579a42ea6f4df5278
Вікторія <viktoriaa@MacBook-Air-Viktoria.local>
2021-06-13 20:32:56 +0300

    steps 1-3

---
+++ contacts-operations/getAll.js
@@ -0,0 +1,15 @@
+const fs = require("fs").promises;
+
+const contactsPath = require("./contactsPath");
+
+const getAllContacts = async () => {
+    try {
+        const file = await fs.readFile(contactsPath);
+        const data = JSON.parse(file);
+        return data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = getAllContacts;
---
+++ contacts-operations/index.js
@@ -0,0 +1,18 @@
+const getAll = require("./getAll");
+const getOneContact = require('./getOneContact')
+const removeContact = require("./removeContact")
+const addContact = require('./addContact')
+
+module.exports = {
+    getAll,
+    getOneContact,
+    removeContact,
+    addContact
+}
+
+// const newContact = {
+//     name: 'Viktoriia',
+//     email: 'khimichv95@gmail.com',
+//     phone: '(093) 008-23-66'
+// }
+
